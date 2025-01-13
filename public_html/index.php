d7f4c77031d1d42753f19746a104fd380b5d04ec
amarcin <102642895+amarcin@users.noreply.github.com>
2025-01-13 12:35:15 -0600

    Refactor .cpanel.yml to update deployment tasks, removing index.html and adding git commands for deployment. Delete index.html and test-outside-folder.txt files.

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $site_title; ?></title>
+</head>
+<body>
+    <h1>Augustin Marcin</h1>
+    <?php 
+    if(isset($_SESSION['user'])) {
+        echo "Welcome back, " . $_SESSION['user'];
+    }
+    ?>
+</body>
+</html> 
